585ed4fd7d9b23ef680b2a43e3ea48933438bf8a
Gabriel Heron <gabrielhcunha2006@gmail.com>
2025-10-15 19:17:45 -0300

    Update Uni5Exe04.java

--- Uni5/Unidade5/src/Uni5Exe04.java
+++ Uni5/Unidade5/src/Uni5Exe04.java
@@ -1,3 +1,23 @@
-public class Uni5Exe04 {
+import java.util.Scanner;
 
+public class Uni5Exe04 {
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        new Uni5Exe04();
+    }
+
+    public Uni5Exe04() {
+        calculo();
+    }
+
+    public void calculo() {
+        double soma = 0;
+        for(int i = 1; i <=20; i++) {
+            double numerador = 3 + (i - 1) * 2;
+            double denominador = i * (i + 1);
+            soma += numerador / denominador;
+        }
+
+        System.out.println("Soma: " + soma);
+    }
 }
